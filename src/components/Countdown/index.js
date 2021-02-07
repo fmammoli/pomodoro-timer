43227d4c96013cd74b6d8beb054e1c9e9331bb31
fmammoli <fmammoli@gmail.com>
2021-02-07 16:55:36 -0300

    removing timer

--- src/components/Countdown/index.js
+++ src/components/Countdown/index.js
@@ -1,86 +1,66 @@
-import React from 'react';
-import Button from '../Button';
+import React from "react";
+import Button from "../Button";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPause, faPlay, faPlus, faStop } from "@fortawesome/free-solid-svg-icons";
-
+import {
+  faPause,
+  faPlay,
+  faPlus,
+  faStop,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Countdown = () => {
+  const workingIntervalDuration = 25 * 60 * 1000;
+  const restIntervalDuration = 5 * 60 * 1000;
 
-  const workingIntervalDuration = 25*60*1000;
-  const restIntervalDuration = 5*60*1000; 
-  
   //const [ ms, setMs ] = React.useState(25*60*1000);
-  const [ ms, setMs ] = React.useState(workingIntervalDuration);
-  const [ timer, setTimer ] = React.useState(null);
+  const [ms, setMs] = React.useState(workingIntervalDuration);
 
   // flag para saber se já está rodando o timer
-  const [ timerActive, setTimerActive ] = React.useState(false); 
+  const [timerActive, setTimerActive] = React.useState(false);
 
   // status: work or rest
-  const [ workingTime, setWorkingTime ] = React.useState(true);
+  const [workingTime, setWorkingTime] = React.useState(true);
 
   function msToMinutesAndSeconds(ms) {
-    var minutes = Math.floor(ms / 60000);
-    var seconds = ((ms % 60000) / 1000).toFixed(0);
-    return (minutes < 10 ? '0' : '' ) + minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
+    return new Date(ms).toISOString().slice(14, -5);
   }
 
-  function updateTimer(ms) {
-    const formattedTime = msToMinutesAndSeconds(ms);
-    setTimer(formattedTime);
-  }
-
-
-  React.useEffect( () => {
-    updateTimer(ms);
-  }, []);
- 
-
-  React.useEffect( () => {
-
-    if(timerActive) {
+  React.useEffect(() => {
+    if (timerActive) {
       const initialTime = Date.now();
-      const intervalFnc = setInterval ( ()=>{
-        const currentTime = Date.now();        
+      const intervalFnc = setInterval(() => {
+        const currentTime = Date.now();
         const accurateDelay = currentTime - initialTime;
-       
-        setMs( ms => ms - accurateDelay);
 
-        if( ms <= 0) {
-          updateTimer(0);
-          if( workingTime ) { 
+        setMs((ms) => ms - accurateDelay);
+
+        if (ms <= 0) {
+          if (workingTime) {
             setMs(restIntervalDuration);
           } else {
             setMs(workingIntervalDuration);
           }
-          setTimeout( () => {
-            setWorkingTime(!workingTime)
+          setTimeout(() => {
+            setWorkingTime(!workingTime);
           }, 1000);
           return;
         }
-        updateTimer(ms);
-        
-
       }, 1000);
 
       return () => {
         clearInterval(intervalFnc);
-      }
-    } else {
-      updateTimer(ms);
+      };
     }
-  }, [timerActive, ms])
+  }, [timerActive, ms]);
 
-  
   function startTimer() {
-    if(!timerActive) {
-      setMs( ms => ms - 1000 );
-      updateTimer(ms);
+    if (!timerActive) {
+      setMs((ms) => ms - 1000);
     }
     setTimerActive(true);
   }
-   
+
   function pauseTimer() {
     setTimerActive(false);
   }
@@ -88,47 +68,44 @@ const Countdown = () => {
   function addOneMin() {
     setMs((ms) => ms + 60000);
   }
-  
+
   function stopTimer() {
     setTimerActive(false);
     setMs(workingIntervalDuration);
-    updateTimer(workingIntervalDuration);
     setWorkingTime(true);
   }
 
   return (
-    <> 
-
-      <p style={{ margin: '0' ,color: 'tomato', textAlign: 'center'}}>
-        {workingTime ? 'work' : 'rest'}
+    <>
+      <p style={{ margin: "0", color: "tomato", textAlign: "center" }}>
+        {workingTime ? "work" : "rest"}
       </p>
 
-      <p style={{ margin: '70px 0', fontSize: '5em', textAlign: 'center' }}>
-        {timer}
+      <p style={{ margin: "70px 0", fontSize: "5em", textAlign: "center" }}>
+        {msToMinutesAndSeconds(ms)}
       </p>
 
-      <div style={{ margin: '20px 0', display: 'flex', justifyContent: 'center'}}>
-        <Button onClick={startTimer} >
+      <div
+        style={{ margin: "20px 0", display: "flex", justifyContent: "center" }}
+      >
+        <Button onClick={startTimer}>
           <FontAwesomeIcon icon={faPlay} />
         </Button>
 
-        <Button onClick={pauseTimer} >
+        <Button onClick={pauseTimer}>
           <FontAwesomeIcon icon={faPause} />
         </Button>
-        
-        <Button onClick={addOneMin} >
+
+        <Button onClick={addOneMin}>
           <FontAwesomeIcon icon={faPlus} />
         </Button>
 
-        <Button onClick={stopTimer} >
+        <Button onClick={stopTimer}>
           <FontAwesomeIcon icon={faStop} />
         </Button>
-
       </div>
-      
     </>
-  )
-
-}
+  );
+};
 
 export default Countdown;
